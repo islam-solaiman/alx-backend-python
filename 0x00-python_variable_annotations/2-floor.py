7c6c822c1e0006f02473fd48afc714f01d583cfd
Islam Solaiman <islam.solaiman92@gmail.com>
2024-01-11 20:55:35 +0000

    Task 2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+''' ype-annotated function floor which takes a float n
+    as argument and returns the floor of the float.
+'''
+
+
+def floor(a: float) -> int:
+    ''' Floor of floating point number '''
+
+    return int(a)
