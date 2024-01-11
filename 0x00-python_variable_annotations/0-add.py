6b47c2deec0c133bffe37f0ca11069dd50d0a251
Islam Solaiman <islam.solaiman92@gmail.com>
2024-01-11 20:44:04 +0000

    Task 0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+''' Taks 0. Basic annotations - add '''
+
+
+def add(a: float, b: float) -> float:
+    '''
+        Add two floating-point numbers.
+    '''
+    return a + b
